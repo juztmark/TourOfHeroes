8d485fd1ecb147ee9cb81e27a646f4ec41d2c2dd
juztmark <miho.marek3@gmail.com>
2020-12-14 12:03:07 +0100

    Deleted unused stuff

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
--- src/app/sort.ts
+++
@@ -1,8 +0,0 @@
-export interface Sort {
-  sortBy: string;
-  sortType: 'asc' | 'desc';
-}
-
-export let itemSortOptions: string[] = ['id', 'name', 'price'];
-export let heroSortOptions: string[] = ['id', 'name', 'money'];
-export let sortTypes: string[] = ['asc', 'desc'];
